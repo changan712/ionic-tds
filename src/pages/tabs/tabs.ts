fed262c1e5f7a3d7c4f60a128f4c037fbef1237d
tinshee <tinshee@sohu.com>
2017-05-11 13:38:54 +0800

    tabs

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {IonicPage, NavController} from 'ionic-angular';
 
 /**
  * Generated class for the Tabs tabs.
@@ -8,18 +8,26 @@ import { IonicPage, NavController } from 'ionic-angular';
  * more info on providers and Angular DI.
  */
 @Component({
-  selector: 'page-tabs',
-  templateUrl: 'tabs.html'
+    selector: 'page-tabs',
+    templateUrl: 'tabs.html'
 })
-@IonicPage({
-
-})
+@IonicPage({})
 export class Tabs {
 
-  tab1Root: any = 'Home';
-  tab2Root: any = 'Feed';
-  tab3Root: any = 'About';
+    selectedIndex: number = 0;
 
-  constructor(public navCtrl: NavController) {}
+    tab1Root: any = 'Home';
+    tab2Root: any = 'Feed';
+    tab3Root: any = 'About';
+
+    constructor(public navCtrl: NavController) {
+        console.log(navCtrl);
+        
+    }
+
+    ionViewCanEnter(){
+        console.log(1)
+        return true;
+    }
 
 }
